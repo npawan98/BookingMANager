c9857c1e93b7c2830cdd0fc2d0dfa9b2b2d20629
Ishaan Agarwal <ishaanagarwal@Ishaans-MacBook-Pro.local>
2021-10-23 17:30:43 +0530

    added top nav bar, bootstrap and working on side nav bar

---
+++ src/components/sideNavBar/sideNav.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class sideNav extends Component{
+    render(){
+        return (
+            <div className = "sideNavBar">
+                This is side nav bar
+            </div>
+        );
+    };
+}
+export default sideNav;
