64064f03de6ec031c4b033fa8e6dee7acb0ecb8e
npawan98 <npawan98@gmail.com>
2021-10-24 11:46:01 +0530

    modified topNavbar

--- src/components/topNavBar/topNav.js
+++ src/components/topNavBar/topNav.js
@@ -7,7 +7,7 @@ class topNav extends Component{
             <div className='topNavBar'>
                 <nav className="navbar navbar-expand-lg navbar-light bg-light">
                     <div className="container-fluid">
-                        <a className="navbar-brand" href="www.google.com">Navbar</a>
+                        <a className="navbar-brand" href="www.google.com">Booking MANager</a>
                         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                         </button>
@@ -17,11 +17,11 @@ class topNav extends Component{
                             <a className="nav-link active" aria-current="page" href="www.google.com">Home</a>
                             </li>
                             <li className="nav-item">
-                            <a className="nav-link" href="www.google.com">Link</a>
+                            <a className="nav-link" href="www.google.com">About us</a>
                             </li>
                             <li className="nav-item dropdown">
                             <a className="nav-link dropdown-toggle" href="www.google.com" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                Dropdown
+                               FAQ's
                             </a>
                             <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
                                 <li><a className="dropdown-item" href="www.google.com">Action</a></li>
@@ -29,9 +29,6 @@ class topNav extends Component{
                                 <li><hr className="dropdown-divider" /></li>
                                 <li><a className="dropdown-item" href="www.google.com">Something else here</a></li>
                             </ul>
-                            </li>
-                            <li className="nav-item">
-                            <a className="nav-link disabled" href="www.google.com">Disabled</a>
                             </li>
                         </ul>
                         <form className="d-flex">
